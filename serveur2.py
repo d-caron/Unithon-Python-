54ac8696e9bead1dc2acebb9835417ec60a53eb1
Raphael Huc <raph82130@gmail.com>
2021-01-28 16:20:12 +0100

    Modification, envoie un Json

--- serveur2.py
+++ serveur2.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify('toto')
+    return {
+        "command": "talk",
+        "ids":["19991606", "19990226"]
+    }
 
 if __name__=="__main__":
     app.run()    
