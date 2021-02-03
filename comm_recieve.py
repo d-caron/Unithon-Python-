d80b83583f6d43f45e9ab075921b9b5c1a386ab2
Raphael Huc <raph82130@gmail.com>
2021-02-03 16:27:48 +0100

    Suppression des rcv

--- comm_recieve.py
+++ comm_recieve.py
@@ -2,7 +2,7 @@ from socket import *
 
 def init_connexion () :
     connexion = socket (AF_INET, SOCK_STREAM)
-    connexion.bind (('localhost', 80))
+    connexion.bind (('localhost', 81))
     connexion.listen (1)
     client_socket, client_address = connexion.accept ()
     return client_socket
@@ -15,7 +15,7 @@ def close_connexion (connexion) :
     print ("sock : connexion closed.")
 
 if __name__=="__main__":
-    connexion = rcv.init_connexion ()
-    msg = rcv.rcv_message (connexion)
+    connexion = init_connexion ()
+    msg = rcv_message (connexion)
     print (">> " + msg)
-    rcv.close_connexion (connexion)
+    close_connexion (connexion)
